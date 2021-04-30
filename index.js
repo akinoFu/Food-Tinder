6abc2469db50fa4c17dd60f0ffbc43d275ed1515
Inggy <71790092+zengingrid@users.noreply.github.com>
2021-04-29 20:49:22 -0700

    added dependencies and folders

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const ejsLayouts = require("express-ejs-layouts");
+const app = express();
+
+require('dotenv').config()
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use(express.json());
+app.set("view engine", "ejs");
+
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+            maxAge: 24 * 60 * 60 * 1000,
+        },
+    })
+);
+
+
+// Middleware
+
+
+// Routes
+
+
+app.listen(3001, function() {
+    console.log(
+        "Server running. Visit: https://localhost:3001/reminders in your browser 🚀"
+    );
+});
