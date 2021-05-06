7f5dbe3dd13520a7b0b29ae5bb9258cbc2d5cb32
akinoFu <akino.fu85@outlook.com>
2021-05-06 12:28:12 -0700

    Update test file

--- database.test.js
+++ database.test.js
@@ -1,22 +1,27 @@
 const db = require("./database/index");
 
-it("return all food", async () => {
-    const result = await db.all();
-    expect(result).toEqual([{ID: 1, FoodName: "Chocolate Donuts"},
-                            {ID: 2, FoodName: "Ramen"},
-                            {ID: 3, FoodName: "Roast Chichen"},
-                            {ID: 4, FoodName: "Pho"},
-                            {ID: 5, FoodName: "Hamburger"}])
+describe("Test db.all - return all food", () => {
+    it("success", async () => {
+        const result = await db.all();
+        expect(result).toEqual([{ID: 1, FoodName: "Chocolate Donuts"},
+                                {ID: 2, FoodName: "Ramen"},
+                                {ID: 3, FoodName: "Roast Chichen"},
+                                {ID: 4, FoodName: "Pho"},
+                                {ID: 5, FoodName: "Hamburger"}])
+    });
 });
 
-it("return a food with an ID", async () => {
-    const result = await db.one(1);
-    expect(result).toEqual([{ID: 1, FoodName: "Chocolate Donuts"}])
+describe("Return a food with an ID", () => {
+    it("ID = 1 (exist)", async () => {
+        const result = await db.one(1);
+        expect(result).toEqual([{ID: 1, FoodName: "Chocolate Donuts"}])
+    });
+    
+    it("ID = 500 (no exist)", async () => {
+        const result = await db.one(500);
+        expect(result).toEqual([])
+    });
 });
 
-it("return a food with an ID (no data)", async () => {
-    const result = await db.one(500);
-    expect(result).toEqual([])
-});
 
 
--- serverRoute.test.js
+++ serverRoute.test.js
@@ -1,4 +1,5 @@
 const app = require("./index");
+const db = require("./database/index");
 const request = require("supertest");
 
 beforeAll((done) => {
@@ -9,74 +10,121 @@ afterAll(async (done) => {
     done();
 });
 
-
 // ========== GET a list of food (/api/food/) ==========
-it("return all food (check the statuscode", async () => {
-    await request(app).get("/api/food")
-    .expect(200)
-});
+describe("GET a list of food (/api/food/) - success", () => {
+    const mock1 = jest.fn()
+    mock1.mockReturnValue([{ID: 1, FoodName: "Chocolate Donuts"},
+                          {ID: 2, FoodName: "Ramen"},
+                          {ID: 3, FoodName: "Roast Chichen"}])
+    db.all = mock1
 
-
-it("return all food", async () => {
-    await request(app).get("/api/food")
-    .expect((response) => {
-        expect(response.body.length).toEqual(5);
+    it("Check the statuscode", async () => {
+        await request(app).get("/api/food")
+        .expect(200);
+    });
+    
+    it("Check the length of the data", async () => {
+        await request(app).get("/api/food")
+        .expect((response) => {
+            expect(response.body.length).toEqual(3);
+        });
+    });
+    
+    it("Check the all records", async () => {
+        await request(app).get("/api/food")
+        .expect((response) => {
+            expect(response.body).toEqual([{ID: 1, FoodName: "Chocolate Donuts"},
+                                           {ID: 2, FoodName: "Ramen"},
+                                           {ID: 3, FoodName: "Roast Chichen"}]);
+        });
     });
 });
 
-it("return all food", async () => {
-    await request(app).get("/api/food")
-    .expect((response) => {
-        expect(response.body[0].ID).toEqual(1);
-        expect(response.body[0].FoodName).toEqual("Chocolate Donuts");
+
+// describe("GET a list of food (/api/food/) - failure", () => {
+    // const mock2 = jest.fn()
+    // const result = new Promise(reject)
+    // mock2.mockReturnValue(result)
+    // db.all = mock2
+
+    // it("Check the statuscode", async () => {
+    //     await request(app).get("/api/food")
+    //     .expect(500);
+    // });
+    
+//     it("Check the length of the data", async () => {
+//         await request(app).get("/api/food")
+//         .expect((response) => {
+//             expect(response.body.length).toEqual(3);
+//         });
+//     });
+    
+//     it("Check the all records", async () => {
+//         await request(app).get("/api/food")
+//         .expect((response) => {
+//             expect(response.body).toEqual([{ID: 1, FoodName: "Chocolate Donuts"},
+//                                            {ID: 2, FoodName: "Ramen"},
+//                                            {ID: 3, FoodName: "Roast Chichen"}]);
+//         });
+//     });
+// });
+
+describe("Test db.all - return all food", () => {
+    // const mock3 = jest.fn()
+    // mock3.mockReturnValue({food: "Chocolate Donuts", photo: "https://8.8.8.8/"})
+    // db.one(1) = mock3
+
+    it("return a food pict (success)", async () => {
+        await request(app).get("/api/food/1")
+        .expect((response) => {
+            expect(response.res.text).toContain("Chocolate Donuts")
+        })
     });
-});
-
-it("return all food", async () => {
-    await request(app).get("/api/food")
-    .expect((response) => {
-        expect(response.body).toEqual([{ID: 1, FoodName: "Chocolate Donuts"},
-                                       {ID: 2, FoodName: "Ramen"},
-                                       {ID: 3, FoodName: "Roast Chichen"},
-                                       {ID: 4, FoodName: "Pho"},
-                                       {ID: 5, FoodName: "Hamburger"}]);
-    });
+    
+    // it("return a food pict (fail)", async () => {
+    //     await request(app).get("/api/food/100")
+    //     .expect((response) => {
+    //         expect(response.res.statusCode).toBe(500)
+    //     })
+    // });
 });
 
 
-// ========== GET a food (/api/food/:id) ==========
-it("return a food pict (success)", async () => {
-    await request(app).get("/api/food/1")
-    .expect((response) => {
-        expect(response.res.text).toContain("Chocolate Donuts")
-    })
-});
 
-// it("return a food pict (fail)", async () => {
-//     await request(app).get("/api/food/100")
-//     .expect((response) => {
-//         expect(response.res.statusCode).toBe(500)
-//     })
+// ========== GET a list of likes (/api/likes/:userId) ==========
+// describe("GET a list of likes (/api/likes/:userId)", async () => {
+//     it("Check the statuscode (The user id exists)", async () => {
+//         await request(app).get("/api/food/likes/1")
+//         .expect(200)
+//     });
+
+//     it("Check the value", async () => {
+//         await request(app).get("/api/food/likes/1")
+//         .expect((response) => {
+//             expect(response.body).toEqual([{ID: 1},
+//                                            {ID: 2},
+//                                            {ID: 5}]);
+//         });
+//     });
+
+//     it("Check the statuscode (The user id does not exist)", async () => {
+//         await request(app).get("/api/food/likes/100")
+//         .expect(500)
+//     });
 // });
 
-
-// ========== GET a list of likes (/api/likes) ==========
-// it("return a list of likes", async () => {
+// ========== GET a list of history (/api/hists/:userId) ==========
+// describe("return a list of history", async () => {
 //     pass
 // });
 
-// ========== GET a list of history (/api/hists) ==========
-// it("return a list of history", async () => {
+// ========== POST a food to a list of like (/api/food/:userId/:id) ==========
+// describe("Add a food to a list of like", async () => {
 //     pass
 // });
 
-// ========== POST a food to a list of like (/api/food/:id) ==========
-// it("Add a food to a list of like", async () => {
-//     pass
-// });
-
-// ========== POST a restaurant the user viewd to a list of hitory (/api/hist/rest/:restaurantId) ==========
-// it("Add a restaurant the user viewd to a list of hitory", async () => {
+// ========== POST a restaurant the user viewd to a list of hitory (/api/hist/rest/:userId/:restaurantId) ==========
+// describe("Add a restaurant the user viewd to a list of hitory", async () => {
 //     pass
 // });
 
