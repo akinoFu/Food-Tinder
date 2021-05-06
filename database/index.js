0ca5ac7cc205697bb0675dac3f44a8e5f091ec7d
Raidemonde <i.am.rahul@live.com>
2021-05-06 12:49:49 -0700

    Added random page button

--- database/index.js
+++ database/index.js
@@ -33,4 +33,9 @@ foodDatabase.one = (id) => {
     });
 };
 
+function randomPage(){
+	let pageInt = Math.floor(Math.random()*100)
+  location.replace(`http://localhost:3000/api/food/${pageInt}`)
+}
+
 module.exports = foodDatabase;
