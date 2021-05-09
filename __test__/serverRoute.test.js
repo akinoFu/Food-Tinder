9f962d23391a05b51261bad5a3eca9985db5ceb7
akinoFu <akino.fu85@outlook.com>
2021-05-09 10:57:06 -0700

    Update test files

--- __test__/serverRoute.test.js
+++ __test__/serverRoute.test.js
@@ -1,45 +1,25 @@
-const app = require("./index");
-const db = require("./database/index");
+const app = require("../index");
+const db = require("../database/index");
 const request = require("supertest");
 
-
-beforeAll((done) => {
-    done();
-});
-
-afterAll(async (done) => {
-    done();
-});
-
 // ========== GET a list of food (/api/food/) ==========
-describe("GET a list of food (/api/food/) - success", () => {
-    const mock1 = jest.fn()
-    mock1.mockReturnValue([{ID: 1, FoodName: "Chocolate Donuts"},
+describe("GET a list of food (/api/food/)", () => {
+    const mockDbAll = jest.fn()
+    mockDbAll.mockReturnValue([{ID: 1, FoodName: "Chocolate Donuts"},
                           {ID: 2, FoodName: "Ramen"},
                           {ID: 3, FoodName: "Roast Chicken"}])
-    db.all = mock1
+    db.all = mockDbAll
 
-    it("Check the statuscode", async () => {
+    it("Check the statuscode (success)", async () => {
         await request(app).get("/api/food")
         .expect(200);
     });
     
-    it("failing to fetch database", async () => {
-        db.all.mockImplementationOnce(() => Promise.reject(error));
+    it("Check the statuscode (failure)", async () => {
+        db.all.mockImplementationOnce(() => Promise.reject());
         await request(app).get("/api/food")
         .expect(500);
       });
-
-    it("Check for existing id", async () => {
-        await request(app).get("/api/food/1")
-        .expect(200);
-    });
-
-    it("Check error for non existing id", async () => {
-        await request(app).get("/api/food/102")
-        .expect(500);
-    });
-
 
     it("Check the length of the data", async () => {
         await request(app).get("/api/food")
@@ -59,65 +39,31 @@ describe("GET a list of food (/api/food/) - success", () => {
 });
 
 
+// ========== GET a food pict (/api/food/:id) ==========
+describe("Get a food pict (/api/food/:id)", () => {
+    const mockDbOne = jest.fn()
+    mockDbOne.mockReturnValue([{ID: 1, FoodName: "Chocolate Donuts"}])
+    db.one = mockDbOne
 
+    it("Check the statuscode (success)", async () => {
+        await request(app).get("/api/food/1")
+        .expect(200);
+    });
 
-
-// describe("Get food ID(/api/food/:id", () => {
-//     const mock2 = jest.fn()
-//     mock2.mockReturnValue()
-//     db.one = mock2
-
-
-// });
-
-// describe("GET a list of food (/api/food/) - failure", () => {
-    // const mock2 = jest.fn()
-    // const result = new Promise(reject)
-    // mock2.mockReturnValue(result)
-    // db.all = mock2
-
-    // it("Check the statuscode", async () => {
-    //     await request(app).get("/api/food")
-    //     .expect(500);
-    // });
-    
-//     it("Check the length of the data", async () => {
-//         await request(app).get("/api/food")
-//         .expect((response) => {
-//             expect(response.body.length).toEqual(3);
-//         });
-//     });
-    
-//     it("Check the all records", async () => {
-//         await request(app).get("/api/food")
-//         .expect((response) => {
-//             expect(response.body).toEqual([{ID: 1, FoodName: "Chocolate Donuts"},
-//                                            {ID: 2, FoodName: "Ramen"},
-//                                            {ID: 3, FoodName: "Roast Chichen"}]);
-//         });
-//     });
-// });
-
-describe("Test db.all - return all food", () => {
-    // const mock3 = jest.fn()
-    // mock3.mockReturnValue({food: "Chocolate Donuts", photo: "https://8.8.8.8/"})
-    // db.one(1) = mock3
-
-    it("return a food pict (success)", async () => {
+    it("Check the rendering data (success)", async () => {
         await request(app).get("/api/food/1")
         .expect((response) => {
             expect(response.res.text).toContain("Chocolate Donuts")
         })
     });
-    
-    // it("return a food pict (fail)", async () => {
-    //     await request(app).get("/api/food/100")
-    //     .expect((response) => {
-    //         expect(response.res.statusCode).toBe(500)
-    //     })
-    // });
-});
 
+    it("Check the statuscode (failure)", async () => {
+        db.one.mockImplementationOnce(() => Promise.reject());
+        await request(app).get("/api/food/1")
+        .expect(500);
+    });
+
+});
 
 
 // ========== GET a list of likes (/api/likes/:userId) ==========
