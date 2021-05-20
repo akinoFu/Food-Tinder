999b8442af277819b879544847b0d0f662d912a7
HoSung Yi <bdoma13@gmail.com>
2021-05-19 22:54:13 -0700

    ymltesting5

--- sql_data/dummy.sql
+++ sql_data/dummy.sql
@@ -14,30 +14,30 @@ INSERT  INTO users VALUES (4, "Ho Sung", "hyi10@my.bcit.ca", "Group7!");
 INSERT  INTO users VALUES (5, "Ingrid", "xzeng11@my.bcit.ca", "Group7!");
 
 
--- DROP TABLE IF EXISTS `likes`;
--- CREATE TABLE likes (
---     userID INT NOT NULL,
--- 	foodID INT NOT NULL,
--- 	foodName VARCHAR(50) NOT NULL,
---     timesLiked INT NOT NULL,
---     PRIMARY KEY (userID, FoodID),
--- 	FOREIGN KEY (userID) REFERENCES users(id),
--- 	FOREIGN KEY (foodID) REFERENCES food_option(ID)
--- );
+DROP TABLE IF EXISTS `likes`;
+CREATE TABLE likes (
+    userID INT NOT NULL,
+	foodID INT NOT NULL,
+	foodName VARCHAR(50) NOT NULL,
+    timesLiked INT NOT NULL,
+    PRIMARY KEY (userID, FoodID),
+	FOREIGN KEY (userID) REFERENCES users(id),
+	FOREIGN KEY (foodID) REFERENCES food_option(ID)
+);
 
--- INSERT  INTO likes VALUES (1, "65", "Chicken Noodle Soup", "2");
--- INSERT  INTO likes VALUES (1, "21", "Crab Cake", "2");
--- INSERT  INTO likes VALUES (1, "99", "Egg Sandwich", "3");
--- INSERT  INTO likes VALUES (1, "5", "Blueberry Muffin","5");
--- INSERT  INTO likes VALUES (1, "12", "Egg Waffle", "1");
--- INSERT  INTO likes VALUES (2, "43", "IceCream", "2");
--- INSERT  INTO likes VALUES (2, "22", "Happy Meal", "2");
--- INSERT  INTO likes VALUES (3, "95", "Croissant", "3");
--- INSERT  INTO likes VALUES (4, "55", "Vanilla Cake", "5");
--- INSERT  INTO likes VALUES (5, "12", "Egg Waffle", "1");
--- INSERT  INTO likes VALUES (5, "32", "Greek Salad", "3");
--- INSERT  INTO likes VALUES (5, "17", "French Toast", "10");
--- INSERT  INTO likes VALUES (5, "16", "Chocolate Waffle", "9");
+INSERT  INTO likes VALUES (1, "65", "Chicken Noodle Soup", "2");
+INSERT  INTO likes VALUES (1, "21", "Crab Cake", "2");
+INSERT  INTO likes VALUES (1, "99", "Egg Sandwich", "3");
+INSERT  INTO likes VALUES (1, "5", "Blueberry Muffin","5");
+INSERT  INTO likes VALUES (1, "12", "Egg Waffle", "1");
+INSERT  INTO likes VALUES (2, "43", "IceCream", "2");
+INSERT  INTO likes VALUES (2, "22", "Happy Meal", "2");
+INSERT  INTO likes VALUES (3, "95", "Croissant", "3");
+INSERT  INTO likes VALUES (4, "55", "Vanilla Cake", "5");
+INSERT  INTO likes VALUES (5, "12", "Egg Waffle", "1");
+INSERT  INTO likes VALUES (5, "32", "Greek Salad", "3");
+INSERT  INTO likes VALUES (5, "17", "French Toast", "10");
+INSERT  INTO likes VALUES (5, "16", "Chocolate Waffle", "9");
 
 
 DROP TABLE IF EXISTS `food_option`;
