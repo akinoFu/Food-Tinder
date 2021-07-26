42990f5baac6507c0ed01a91c92b44b2d358d316
Inggy <71790092+zengingrid@users.noreply.github.com>
2021-07-25 19:18:58 -0700

    working google api

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const restaurantRouter = require('./routes/restaurantRoute');
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
     session({
--- routes/restaurantRoute.js
+++ routes/restaurantRoute.js
@@ -7,7 +7,7 @@ const fetch  = require("node-fetch")
 /* Show a list of restaurants for a food item */
 router.get('/:food', ensureAuthenticated, async(req, res, next) => {
     try {
-        let clientID = "AIzaSyB8xBoyF_g2_rciDFC9sgx43EUgE_oRQug";
+        let clientID = "AIzaSyBCf63k5kDJVoymppDpviycoU2czteAZUo";
         let query = req.params.food;
         let userID = await req.session.passport.user;
         res.render("./restaurant", {apiID: clientID, query: query, id: userID});
